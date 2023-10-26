6163fd838c24bea7b539c07786efd825cdfa0525
Haky86 <hakamelassouad@gmail.com>
2023-10-27 01:43:11 +0200

    sm6150-common: Build bluetooth stack from source
    Change-Id: Ib9ff0f221ee6b089668667687429312397bc0cb6

--- sm6150.mk
+++ sm6150.mk
@@ -126,7 +126,14 @@ PRODUCT_PACKAGES += \
 
 # Bluetooth
 PRODUCT_PACKAGES += \
-    android.hardware.bluetooth@1.0 \
+    android.hardware.bluetooth@1.0.vendor \
+    audio.bluetooth.default \
+    com.qualcomm.qti.bluetooth_audio@1.0 \
+    com.qualcomm.qti.bluetooth_audio@1.0.vendor \
+    libbluetooth_audio_session \
+    vendor.qti.hardware.bluetooth_audio@2.0.vendor \
+    vendor.qti.hardware.btconfigstore@1.0.vendor \
+    vendor.qti.hardware.btconfigstore@2.0.vendor \
 
 # Camera
 PRODUCT_PACKAGES += \
