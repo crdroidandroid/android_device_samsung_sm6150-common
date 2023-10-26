c34f35491a73544c2a8bf109d996e2f1e3463ccd
Roshan Pius <rpius@google.com>
2023-10-27 01:27:33 +0200

    sm6150-common: Add WifiOverlay
    Bug: 148617260
    Test: Device boots up and connects to wifi networks.
    Change-Id: I200520b93cc80e8f9eaab78ef394170337f1604d

--- sm6150.mk
+++ sm6150.mk
@@ -340,6 +340,10 @@ PRODUCT_SOONG_NAMESPACES += \
     $(COMMON_PATH) \
 
 # WiFi
+PRODUCT_PACKAGES += \
+    TetheringConfigOverlay \
+    WifiOverlay \
+
 PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/rootdir/vendor/etc/wifi/icm.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/icm.conf \
     $(COMMON_PATH)/rootdir/vendor/etc/wifi/indoorchannel.info:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/indoorchannel.info \
