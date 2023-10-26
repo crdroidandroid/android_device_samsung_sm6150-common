b3f82a2d55e5b75b16a793b3d3bdfd7d1e054097
Ameya Thakur <ameyat@codeaurora.org>
2023-10-27 01:10:24 +0200

    sm6150-common: Generate non-hlos image mount points
    * Needed for A/B builds.
    * On builds where A/B support is enabled the system image acts as the rootfs.
    * In this case we can no longer create the non-hlos image mount points at run time as we used to.
    * We now create them as part of the build itself.
    * As for the non A/B cases it does not hurt having them created at build time and simplifies selinux rules.
    
    Change-Id: Ib02b4948c3f6c95b9fd92de844bc820ababc2efb

--- Android.mk
+++ Android.mk
@@ -29,4 +29,31 @@ ifneq ($(filter a60q a70q a70s a71 m40dd m51 r1q,$(TARGET_DEVICE)),)
 
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
+include $(CLEAR_VARS)
+
+# A/B builds require us to create the mount points at compile time.
+# Just creating it for all cases since it does not hurt.
+BT_FIRMWARE_MOUNT_POINT := $(TARGET_OUT_VENDOR)/bt_firmware
+DSP_MOUNT_POINT := $(TARGET_OUT_VENDOR)/dsp
+FIRMWARE_MOUNT_POINT := $(TARGET_OUT_VENDOR)/firmware_mnt
+MODEM_MOUNT_POINT := $(TARGET_OUT_VENDOR)/firmware-modem
+
+$(BT_FIRMWARE_MOUNT_POINT):
+	@echo "Creating $(BT_FIRMWARE_MOUNT_POINT)"
+	@mkdir -p $(TARGET_OUT_VENDOR)/bt_firmware
+
+$(DSP_MOUNT_POINT):
+	@echo "Creating $(DSP_MOUNT_POINT)"
+	@mkdir -p $(TARGET_OUT_VENDOR)/dsp
+
+$(FIRMWARE_MOUNT_POINT):
+	@echo "Creating $(FIRMWARE_MOUNT_POINT)"
+	@mkdir -p $(TARGET_OUT_VENDOR)/firmware_mnt
+
+$(MODEM_MOUNT_POINT): $(LOCAL_INSTALLED_MODULE)
+	@echo "Creating $(MODEM_MOUNT_POINT)"
+	@mkdir -p $(TARGET_OUT_VENDOR)/firmware-modem
+
+ALL_DEFAULT_INSTALLED_MODULES += $(BT_FIRMWARE_MOUNT_POINT) $(DSP_MOUNT_POINT) $(FIRMWARE_MOUNT_POINT) $(MODEM_MOUNT_POINT)
+
 endif
