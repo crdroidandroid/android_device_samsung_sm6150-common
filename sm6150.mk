821ee5631a8161e8676b3140d1ef41782e3338de
Jeff Gaston <jeffrygaston@google.com>
2023-10-27 01:07:52 +0200

    sm6150-common: Create a device specific Soong namespace
    Change-Id: If7bfeac1c9edacaaed150ff1b0024a4fcfb3de94

--- sm6150.mk
+++ sm6150.mk
@@ -152,5 +152,9 @@ PRODUCT_PACKAGES += \
     android.hardware.radio.config@1.2 \
     libxml2 \
 
+# Soong Namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(COMMON_PATH) \
+
 # Get non-open-source specific aspects
 $(call inherit-product, vendor/samsung/sm6150-common/sm6150-common-vendor.mk)
