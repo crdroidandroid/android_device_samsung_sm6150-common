819bafab89d46d0e4160813ac67b643d2ff38295
CrisBal <74150843+CBG-leeco@users.noreply.github.com>
2023-10-27 02:08:14 +0200

    sm6150-common: Build mock hidl power stats
    * Fix for hidl power stats errors.
    - Now with this we fixing both errors:
    BatteryStatsService: Unable to load Power Hal or power.stats HAL
    hwservicemanager: getTransport: Cannot find entry android.hardware.power.stats@1.0::IPowerStats/default in either framework or device manifest.
    hwservicemanager: getTransport: Cannot find entry android.hardware.power@1.0::IPower/default in either framework or device manifest.
    
    Change-Id: Ib84958962fdaa0cb945cb9fe988cec990012361e

--- sm6150.mk
+++ sm6150.mk
@@ -471,6 +471,7 @@ PRODUCT_COPY_FILES += \
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service-qti.sm6150 \
+    android.hardware.power.stats@1.0-service.mock \
 
 PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/rootdir/vendor/etc/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml \
