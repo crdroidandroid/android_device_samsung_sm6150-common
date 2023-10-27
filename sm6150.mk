1f9083829eb9b0fe361b5bb4c15026e834da9a58
Smita Ghosh <smitag@codeaurora.org>
2023-10-27 09:31:49 +0200

    sm6150-common: Build the vendor service manager from source
    * This is now added to PRODUCT_PACKAGES based on the
      shipping API, and on API 30+ it's no longer built
      but very few targets actually support not using it.
      https://source.android.com/devices/architecture/hidl/binder-ipc
    
    Change-Id: I0d25fa9bc3f1342139153b54c7cca71cb41b1d38

--- sm6150.mk
+++ sm6150.mk
@@ -651,6 +651,10 @@ PRODUCT_PACKAGES += \
     android.hardware.usb@1.0.vendor \
     android.hardware.usb@1.1.vendor \
 
+# Vendor Service Manager
+PRODUCT_PACKAGES += \
+    vndservicemanager \
+
 # WiFi
 PRODUCT_PACKAGES += \
     TetheringConfigOverlay \
