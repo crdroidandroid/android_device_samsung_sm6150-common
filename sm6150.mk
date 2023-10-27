ebb4a2b6e6d00616ec5e09a2932eb7a17647a0ee
Ataberk Özen <ataberkozen123@gmail.com>
2023-10-27 09:29:50 +0200

    sm6150-common: Build the QTI thermal 2.0 libs from source
    Change-Id: Id8fdce75347dd58ea5afce52699cc52c77349060

--- sm6150.mk
+++ sm6150.mk
@@ -629,6 +629,10 @@ PRODUCT_BOOT_JARS += \
 PRODUCT_PACKAGES += \
     textclassifier.bundle1 \
 
+# Thermal
+PRODUCT_PACKAGES += \
+    android.hardware.thermal@2.0-service.qti \
+
 # WiFi
 PRODUCT_PACKAGES += \
     TetheringConfigOverlay \
