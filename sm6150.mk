550ab03a33621c7bb50055c7e39f4afe0abb2b5d
danascape <danascape@gmail.com>
2023-10-27 02:01:28 +0200

    sm6150-common: Build libavservices_minijail from source
    Change-Id: I0e7876db464d13f1d3f9f3ab5a13ff4c86f0380d

--- sm6150.mk
+++ sm6150.mk
@@ -329,6 +329,10 @@ PRODUCT_PACKAGES += \
     lights.sm6150 \
 
 # Media
+PRODUCT_PACKAGES += \
+    libavservices_minijail \
+    libavservices_minijail.vendor \
+
 PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/rootdir/vendor/etc/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
     $(COMMON_PATH)/rootdir/vendor/etc/media_codecs_c2_sec.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_c2_sec.xml \
