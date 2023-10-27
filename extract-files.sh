0dcacdd2c451d5050c00b09c15a2a1482a49ffef
LuK1337 <priv.luk@gmail.com>
2023-10-27 12:49:38 +0200

    sm6150-common: Pass call state to audio hal and patch RIL
    * Fixes 2nd sim audio when in call.
    
    Change-Id: I1ab3f27739db84d97f760930fd1befa82184d46f

--- extract-files.sh
+++ extract-files.sh
@@ -30,6 +30,17 @@ if [ ! -f "${HELPER}" ]; then
 fi
 source "${HELPER}"
 
+function blob_fixup() {
+    case "${1}" in
+        vendor/lib64/libsec-ril.so)
+            sed -i 's/ril.dds.call.slotid/vendor.calls.slotid/g' "${2}"
+            ;;
+        vendor/lib64/libsec-ril-dsds.so)
+            sed -i 's/ril.dds.call.slotid/vendor.calls.slotid/g' "${2}"
+            ;;
+    esac
+}
+
 # Default to sanitizing the vendor folder before extraction
 CLEAN_VENDOR=true
 
