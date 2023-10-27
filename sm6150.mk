b43e3f6be8923b8a8fc6c962a739a1afe5d8a5a1
josip-k <d3hum4niz3d@gmail.com>
2023-10-27 09:23:49 +0200

    sm6150-common: Build the Samsung sensors 2.1 multihal from source
    * Built from hardware/samsung.
    
    Change-Id: Ic506adf708ec962c89b933bf80055c4fb163a731

--- sm6150.mk
+++ sm6150.mk
@@ -588,7 +588,8 @@ PRODUCT_COPY_FILES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    android.hardware.sensors@2.0-service.multihal \
+    android.hardware.sensors@2.1-service.samsung-multihal \
+    android.hardware.sensors@2.0-ScopedWakelock.vendor \
     libsensorndkbridge \
 
 PRODUCT_COPY_FILES += \
