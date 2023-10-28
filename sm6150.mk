8bdae978ce2c7470e990295c334c239ae0344bef
Simon1511 <simon2002.schoenmackers@gmail.com>
2023-10-28 23:15:10 +0200

    sm6150-common: Build Lineage Touch HAL from source
    Change-Id: I186a5fb962b6ef2f836d460429fbd162d7c51b6b

--- sm6150.mk
+++ sm6150.mk
@@ -598,7 +598,7 @@ PRODUCT_PACKAGES += \
 
 # Touch
 PRODUCT_PACKAGES += \
-    vendor.lineage.touch@1.0-service.samsung \
+    vendor.lineage.touch@1.0-service.sm6150 \
 
 # Trust
 PRODUCT_PACKAGES += \
