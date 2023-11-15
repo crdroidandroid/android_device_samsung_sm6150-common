49008829b108add3fcdddd282abf3daa4edc3aee
Simon1511 <simon2002.schoenmackers@gmail.com>
2023-11-16 00:22:24 +0100

    sm6150-common: sepolicy: Address system_suspend denials
    Change-Id: Ib31f42c8e838e7e123a5f23f3d58d3f559e0bc3b

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -287,7 +287,8 @@ TARGET_SEPOLICY_DIR := msmsteppe
 include device/qcom/sepolicy_vndr-legacy-um/SEPolicy.mk
 
 BOARD_VENDOR_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
-SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/public
+PRODUCT_PRIVATE_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/private
+PRODUCT_PUBLIC_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/public
 
 # Treble
 BOARD_VNDK_VERSION := current
