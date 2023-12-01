ec0ed3f33a8779ad6a742ce9d2a71b9476a21b7d
LuK1337 <priv.luk@gmail.com>
2023-12-02 00:15:24 +0100

    sm6150-common: Build android.frameworks.sensorservice@1.0.vendor from source
    * Required by mm-pp-deps.
    
    Change-Id: I7061de0923c9c2c9496c6ce71766b1285bca41ff

--- sm6150.mk
+++ sm6150.mk
@@ -556,6 +556,7 @@ PRODUCT_COPY_FILES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
+    android.frameworks.sensorservice@1.0.vendor \
     android.hardware.sensors-service.samsung-multihal \
     android.hardware.sensors@2.0-ScopedWakelock.vendor \
     libsensorndkbridge \
