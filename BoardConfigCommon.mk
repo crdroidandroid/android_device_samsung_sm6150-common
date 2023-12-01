1a0fa23d50878ca1c8178e68160cca866495bced
Chenyang Zhong <zhongcy95@gmail.com>
2023-12-02 00:00:42 +0100

    sm6150-common: Specify androidboot.boot_devices
    * So that /dev/block/by-name/ symlinks are created.
    
    Change-Id: I46ef26801d70dc14ac43621ee473cbf2f35182c0

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -130,6 +130,7 @@ DEVICE_MATRIX_FILE := \
 # Kernel
 BOARD_INCLUDE_DTB_IN_BOOTIMG := true
 BOARD_KERNEL_BASE := 0x00000000
+BOARD_KERNEL_CMDLINE += androidboot.boot_devices=soc/1d84000.ufshc
 BOARD_KERNEL_CMDLINE := console=null androidboot.hardware=qcom androidboot.memcg=1 lpm_levels.sleep_disabled=1 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 service_locator.enable=1 swiotlb=1 androidboot.usbcontroller=a600000.dwc3 firmware_class.path=/vendor/firmware_mnt/image nokaslr printk.devkmsg=on
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 TARGET_KERNEL_APPEND_DTB := true
