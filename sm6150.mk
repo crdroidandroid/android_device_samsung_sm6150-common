072b8cbfb58310d8f461b27d0db25f042dd88c93
Remi NGUYEN VAN <reminv@google.com>
2023-12-06 18:19:15 +0100

    sm6150-common: rro_overlays: Add connectivity overlay
    As resources were moved from the android package to the connectivity
    resources package, migrate resources to a new overlay for the new
    package.
    
    Bug: 185850634
    Test: m
    Change-Id: Ib3776912741da5b8e9cd0138c0de2cc5a4ccf83d

--- sm6150.mk
+++ sm6150.mk
@@ -618,6 +618,7 @@ PRODUCT_PACKAGES += \
 # WiFi
 PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service \
+    ConnectivityOverlay \
     hostapd \
     libwifi-hal-qcom \
     libwpa_client \
